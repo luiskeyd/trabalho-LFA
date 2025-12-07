8bbe6907de8bd6e2ffe0941e8248005e24de9d3d
luiskeyd <luiseduardofonseca63@gmail.com>
2025-12-07 11:41:02 -0300

    corrige automato.py

--- automato.py
+++ automato.py
@@ -6,22 +6,21 @@ class Automato:
     self.estado_inicial = estado_inicial
     self.estados_finais = estados_finais
 
-    def __str__(self):
-      return f"""
-        Autômato:
-        - Estados: {sorted(self.estados)}
-        - Alfabeto: {sorted(self.alfabeto)}
-        - Estado Inicial: {self.estado_inicial}
-        - Estados Finais: {sorted(self.estados_finais)}
-        - Transições: {dict(self.transcoes)}
-      """
+  def __str__(self):
+    return f"""
+      Autômato:
+      - Estados: {self.estados}
+      - Alfabeto: {sorted(self.alfabeto)}
+      - Estado Inicial: {self.estado_inicial}
+      - Estados Finais: {self.estados_finais}
+      - Transições: {self.transicoes}
+    """
     
-
 class AFN(Automato):
   pass
 
-class AFe(Automato):
+class AFD(Automato):
   pass
 
-class AFD(Automato):
+class AFe(Automato):
   pass
